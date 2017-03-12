46f2e9e85c694f1e63071b142aa24e59fe3abc60
Vincent Ollivier <v@vinc.cc>
2017-03-12 10:24:02 +0100

    Add hackernews keywords search

--- app/models/hackernews_item.rb
+++ app/models/hackernews_item.rb
@@ -1,17 +1,17 @@
 class HackernewsItem < Item
   def self.from_hash(hash)
-    via = "https://news.ycombinator.com/item?id=#{hash['id']}"
-    url = hash['url'].start_with?('item?id=') ? via : hash['url']
+    via = "https://news.ycombinator.com/item?id=#{hash['id'] || hash['objectID']}"
+    url = hash['url'].nil? || hash['url'].start_with?('item?id=') ? via : hash['url']
 
     self.new(
-      author: hash['user'],
-      created_at: Time.at(hash['time']),
+      author: hash['user'] || hash['author'],
+      created_at: Time.at(hash['time'] || hash['created_at_i']),
       title: hash['title'],
       url: url,
       via: via,
       counts: Counts.new(
         points: hash['points'],
-        comments: hash['comments_count']
+        comments: hash['comments_count'] || hash['num_comments']
       )
     )
   end
--- app/models/hackernews_source.rb
+++ app/models/hackernews_source.rb
@@ -4,8 +4,16 @@ class HackernewsSource < Source
     @url = 'https://news.ycombinator.com'
   end
 
-  # https://github.com/cheeaun/node-hnapi/wiki/API-Documentation
   def request(args, options={})
+    items = args.size > 1 ? request_search(args, options) : request_homepage(options)
+
+    items.map do |item|
+      HackernewsItem.from_hash(item)
+    end
+  end
+
+  # https://github.com/cheeaun/node-hnapi/wiki/API-Documentation
+  def request_homepage(options={})
     limit = (1..30).include?(options[:limit]) ? options[:limit] : 30
 
     sorts = %i(new hot top)
@@ -16,10 +24,31 @@ class HackernewsSource < Source
     items = JSON.parse(res.body)
     items = items.sort_by { |item| -(item['points'] || 0) } if sort == :top
     items = items.sort_by { |item| -item['time'] } if sort == :new
-    items = items.take(limit)
 
-    items.map do |item|
-      HackernewsItem.from_hash(item)
-    end
+    items.take(limit)
+  end
+
+  # https://hn.algolia.com/api
+  def request_search(args, options={})
+    limit = (1..100).include?(options[:limit]) ? options[:limit] : 10
+
+    # NOTE: only used by top and controversial sorts
+    times = %i(hour day week month year all)
+    opt = options[:t] || options[:time]
+    time = times.include?(opt) ? opt : :day
+    created_after = time == :all ? 0 : (Time.zone.now - 1.send(time)).to_i
+
+    params = {
+      query: args[1..-1].join(' '),
+      tags: 'story',
+      hitsPerPage: limit,
+      numericFilters: "created_at_i>#{created_after}"
+    }
+
+    url = 'http://hn.algolia.com/api/v1/search'
+    res = RestClient.get(url, params: params)
+    json = JSON.parse(res.body)
+
+    json['hits']
   end
 end
