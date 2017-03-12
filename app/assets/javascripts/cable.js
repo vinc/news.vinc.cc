a47127c8370b140a0b8e227213defd808473a2a3
Vincent Ollivier <v@vinc.cc>
2017-03-12 21:33:08 +0100

    Avoid creating websockets connections without token

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,11 +8,15 @@
 (function() {
   this.App || (this.App = {});
 
+  var id = store.get('auth_id');
   var key = store.get('auth_secret');
-  var msg = 'GET /cable/';
-  var mac = CryptoJS.HmacSHA256(msg, key);
-  var token = store.get('auth_id') + ':' + mac;
 
-  App.cable = ActionCable.createConsumer('/cable/?token=' + token);
+  if (id && key) {
+    var msg = 'GET /cable/';
+    var mac = CryptoJS.HmacSHA256(msg, key);
+    var token = id + ':' + mac;
+
+    App.cable = ActionCable.createConsumer('/cable/?token=' + token);
+  }
 
 }).call(this);
