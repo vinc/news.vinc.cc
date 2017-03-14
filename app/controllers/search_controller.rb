a7715ae67a70cb1a05d780fcd94b9865658006be
Vincent Ollivier <v@vinc.cc>
2017-03-14 10:36:40 +0100

    Add autocomplete source guessing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,17 +2,25 @@ class SearchController < ApplicationController
   def index
     expires_in 5.minutes, public: true
 
+    set_query
     set_search(:search)
   end
 
   def autocomplete
     expires_in 1.hour, public: true
 
+    set_query
     set_search(:autocomplete)
 
     if @source.nil?
       sources = Source.all.map { |s| s.new.to_s }
       @results = sources.keep_if { |s| s.starts_with?(@query) }
+
+      if @results.count == 1
+        # Autocomplete from the only source available
+        @query = @results.first
+        set_search(:autocomplete)
+      end
     end
 
     render json: @results.delete_if { |s| s.strip == @query.strip }
@@ -20,8 +28,11 @@ class SearchController < ApplicationController
 
   private
 
-  def set_search(search_type)
+  def set_query
     @query = search_params[:q] || ''
+  end
+
+  def set_search(search_type)
     @source = Source.from_s(@query[/\w+/])
     if @source
       @query[/\w+/] = @source.to_s # Resolve shortcut notation
