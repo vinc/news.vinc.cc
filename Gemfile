5ab798049834796a502b8fd693a35e5eedd38f03
Vincent Ollivier <v@vinc.cc>
2020-01-31 10:22:06 +0100

    Upgrade to rails 5.1 (#62)
    * Upgrade rails gem
    
    * Run rails app:update
    
    * Add new files
    
    * Update framework defaults
    
    * Avoid configuring active_record

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 ruby '2.6.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.1.7'
 # Use Puma as the app server
 gem 'puma', '~> 4.3'
 # Use SCSS for stylesheets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,14 +16,14 @@ Rails.application.configure do
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    #config.cache_store = :memory_store
+    config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
-    #config.cache_store = :null_store
+    config.cache_store = :null_store
   end
 
   # Don't care if the mailer can't send.
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  # config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
